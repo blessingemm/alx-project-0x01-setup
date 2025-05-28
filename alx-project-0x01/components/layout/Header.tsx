0b4017b1091eafdebef74b6c8c7a2fb24465a169
Blessing <eblessing793577@gmail.com>
2025-05-28 13:24:44 +0100

    Update Header and pages for navigation task

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -2,13 +2,19 @@ import Link from 'next/link'
 
 const Header: React.FC = () => {
   return (
-    <header className = "bg-blue-600 text-white shadow-md py-4">
-      <div className = "container mx-auto flex justify-between items-center px-4">
-        <h3 className = "font-bold text-2xl"> <link href="" />Daily Contents</h3>
+    <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h3 className="font-bold text-2xl">
+          <Link href="/">Daily Contents</Link>
+        </h3>
         <nav>
-          <ul className = "flex space-x-6">
-            <li className = "hover:underline"> <link href="/pages/posts/index.tsx"/>Posts</li>
-            <li className = "hover:underline"> <link href="/pages/users/index.tsx"/>Users</li>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/users">Users</Link>
+            </li>
           </ul>
         </nav>
       </div>
